55b50be7315ac9070879ede82e62f2c5281e2d54
sareth <sareth@emp-513-02.oat.local>
2015-06-13 01:31:34 +0700

    Signed-off-by: sareth <sareth@emp-513-02.oat.local>

--- autoParser/src/ru/program_code/sareth/Sloth/autoParser/Main/Main.java
+++ autoParser/src/ru/program_code/sareth/Sloth/autoParser/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 		SearchQueryesImpl a = new SearchQueryesImpl();
 		SearchQueryes b = a.getElementById(1);
-		System.out.println(b.getURL() + " Миша какуля");
+		System.out.println();
 	}
 
 }
